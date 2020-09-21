d8a0085620dccf6e218673faa0853de5e72fefee
yaoxiaobing <xiaobing.yao@duobei.com>
2020-09-21 14:36:48 +0800

    feat: modify macos script

--- build-x264-macos.sh
+++ build-x264-macos.sh
@@ -10,12 +10,14 @@ CONFIGURE_FLAGS="--enable-static --enable-pic --disable-cli --disable-asm"
 Output="$SCRIPT_DIR/x264-macos"
 mkdir -p $Output
 
+CFLAGS="-arch x86_64 -fvisibility=hidden  -fembed-bitcode"
+
 echo $Output
 
 cd x264
 make clean
 
-./configure $CONFIGURE_FLAGS --prefix=$Output
+./configure $CONFIGURE_FLAGS  --prefix=$Output --extra-cflags="$CFLAGS"
 
 make -j `nproc`
 make install
